76bc0bc4bedbe34faa79b89414222f5d7bdc8ce3
nnagaraju@gmail.com <nnagaraju@gmail.com>
2026-01-21 20:30:08 -0500

    Driver Code

--- src/autons.cpp
+++ src/autons.cpp
@@ -15,9 +15,9 @@ const int SWING_SPEED = 110;
 ///
 void default_constants() {
   // P, I, D, and Start I
-  chassis.pid_drive_constants_set(20.0, 0.0, 20.0);         // Fwd/rev constants, used for odom and non odom motions
+  chassis.pid_drive_constants_set(20.0, 0.0, 100.0);         // Fwd/rev constants, used for odom and non odom motions
   chassis.pid_heading_constants_set(11.0, 0.0, 20.0);        // Holds the robot straight while going forward without odom
-  chassis.pid_turn_constants_set(5.0, 0.05, 30.0, 15.0);     // Turn in place constants
+  chassis.pid_turn_constants_set(3.0, 0.05, 20.0, 15.0);     // Turn in place constants
   chassis.pid_swing_constants_set(6.0, 0.0, 65.0);           // Swing constants
   chassis.pid_odom_angular_constants_set(6.5, 0.0, 52.5);    // Angular control for odom motions
   chassis.pid_odom_boomerang_constants_set(5.8, 0.0, 32.5);  // Angular control for boomerang motions
@@ -66,7 +66,15 @@ void highGoal() {
 ///
 void drive_example() {
 
+  highGoalHood.set(true);
   
+  chassis.pid_odom_set(18.5_in, DRIVE_SPEED, false);
+  chassis.pid_wait();
+
+  pros::delay(200); 
+  
+  chassis.pid_turn_set(-90_deg, TURN_SPEED);
+  chassis.pid_wait(); 
 
   
   
--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 ez::Drive chassis(
     // These are your drive motors, the first motor is used for sensing!
     {-10, 9, -8},     // Left Chassis Ports (negative port will reverse it!)
-    {1, -2, 3,},  // Right Chassis Ports (negative port will reverse it!)
+    {1, -2, 3},  // Right Chassis Ports (negative port will reverse it!)
 
     11,      // IMU Port
     2.75,  // Wheel Diameter (Remember, 4" wheels without screw holes are actually 4.125!)
